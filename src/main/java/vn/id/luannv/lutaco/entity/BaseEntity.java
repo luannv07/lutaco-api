e82c95a3aace8a36cfb1eb452d9f41725caaf9b7
luannv <vanluan.dev@gmail.com>
2026-01-02 15:37:10 +0700

    update(base entity): update base entity structure

--- src/main/java/vn/id/luannv/lutaco/entity/BaseEntity.java
+++ src/main/java/vn/id/luannv/lutaco/entity/BaseEntity.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.experimental.FieldDefaults;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
 
 import java.io.Serializable;
 import java.time.LocalDate;
@@ -14,13 +16,21 @@ import java.time.LocalDate;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @MappedSuperclass
 public abstract class BaseEntity implements Serializable {
-    @Column(name = "created_at")
+    @Column(name = "created_date")
     @CreationTimestamp
-    LocalDate createdAt;
+    LocalDate createdDate;
 
-    @Column(name = "updated_at")
+    @Column(name = "updated_date")
     @UpdateTimestamp
-    LocalDate updatedAt;
+    LocalDate updatedDate;
+
+    @Column(name = "created_by")
+    @CreatedBy
+    String createdBy;
+
+    @Column(name = "updated_by")
+    @LastModifiedBy
+    String updatedBy;
 
     @Column(name = "deleted_at", columnDefinition = "datetime default null")
     LocalDate deletedAt; // after set statusCd = 0 (disabled), then use @Scheduled to hard delete after x days
