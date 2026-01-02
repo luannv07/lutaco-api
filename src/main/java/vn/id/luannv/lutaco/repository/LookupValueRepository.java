1bcff990bcc2bf04de9d72ee8f006c1ea1c0ff85
luannv <vanluan.dev@gmail.com>
2026-01-02 16:50:41 +0700

    feat(lookup value): create APIs to find lookup

--- src/main/java/vn/id/luannv/lutaco/repository/LookupValueRepository.java
+++ src/main/java/vn/id/luannv/lutaco/repository/LookupValueRepository.java
@@ -1,9 +1,45 @@
 package vn.id.luannv.lutaco.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import vn.id.luannv.lutaco.dto.request.LookupValueFilterRequest;
 import vn.id.luannv.lutaco.entity.LookupValue;
 
+import java.util.Optional;
+
 @Repository
-public interface LookupValueRepository  extends JpaRepository<LookupValue,Long> {
+public interface LookupValueRepository extends JpaRepository<LookupValue, Long> {
+
+    @Query("""
+        SELECT lv
+        FROM LookupValue lv
+        WHERE
+            (:#{#request.lookupValue} IS NULL 
+                OR lv.lookupValue LIKE %:#{#request.lookupValue}%)
+        AND
+            (:#{#request.lookupGroup} IS NULL 
+                OR lv.lookupGroup = :#{#request.lookupGroup})
+        AND
+            (:#{#request.statusFlg} IS NULL 
+                OR lv.statusFlg = :#{#request.statusFlg})
+    """)
+    Page<LookupValue> findAllByFilters(
+            @Param("request") LookupValueFilterRequest request,
+            Pageable pageable
+    );
+
+    @Query("""
+    SELECT lv FROM LookupValue lv
+    WHERE (lv.lookupValue is null or lv.lookupValue = :lookupValue)
+      or (:lookupGroup IS NULL OR lv.lookupGroup = :lookupGroup)
+""")
+    Optional<LookupValue> findByValueAndOptionalGroup(
+            @Param("lookupValue") String lookupValue,
+            @Param("lookupGroup") String lookupGroup
+    );
+
 }
